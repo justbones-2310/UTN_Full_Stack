a8b3434161730ffaedecaa1d41b8b7aab8c176b2
Bones <justbones2310@gmail.com>
2024-11-18 01:00:08 -0300

    ejercicios M5-U3

--- homework/M5U3/app.js
+++ homework/M5U3/app.js
@@ -25,10 +25,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-/* ejemplo para comunicarme con la bs */
-pool.query('select * from empleados').then(function (resultados) {
+/* EJEMPLOS PARA COMUNICARME CON LA BD
+pool.query('select nombre from empleados').then(function (resultados) {
   console.log(resultados)
 });
+
+var obj = {
+  nombre: 'Salsita',
+  actividad: 'Ex Lider de los Guardianes de la Vagancia',
+  cumpleaños: ("2013-02-08"),
+  edad: 10,
+  mail: 'salsita@gorditos.com'
+};
+pool.query('insert into home set ?', [obj]).then(function (resultados) {
+  console.log(resultados)
+});
+
+var id = 7;
+var obj = {
+  id: 4,
+};
+
+pool.query('update home set ? where id=?', [obj, id]).then(function (resultados) {
+  console.log(resultados);
+})
+
+var id = 9;
+pool.query('delete from home where id=?', [id]).then(function (resultados) {
+  console.log(resultados)
+})*/
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
