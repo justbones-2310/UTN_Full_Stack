6009cdbfef683ef027d40ed1999da5395dd93d94
Bones <justbones2310@gmail.com>
2024-11-18 00:10:06 -0300

    comunicandome con la bd

--- homework/M5U3/app.js
+++ homework/M5U3/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var pool = require('./models/bd');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+/* ejemplo para comunicarme con la bs */
+pool.query('select * from empleados').then(function (resultados) {
+  console.log(resultados)
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
